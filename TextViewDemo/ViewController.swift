9c5e04b1b3e711211ddc5d09a670c43b11c6e42e
Shariif Islam <islamshariif@gmail.com>
2017-06-06 09:33:13 +0600

    push to master

--- TextViewDemo/ViewController.swift
+++ TextViewDemo/ViewController.swift
@@ -9,17 +9,74 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lb_count: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textView.delegate = self
+        
+        textView.layer.borderWidth = 0.5
+        textView.layer.borderColor = UIColor.darkGray.cgColor
+        textView.layer.cornerRadius = 5
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(updateTexView(notification:)), name: Notification.Name.UIKeyboardWillChangeFrame , object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(updateTexView(notification:)), name: Notification.Name.UIKeyboardWillHide , object: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        // this way can be apply if you have multiple textview.
+        //textView.endEditing(true)
+        textView.resignFirstResponder()
     }
+    
+    
+    //Adjust textview text when keyboard appear and keep typing
+    func updateTexView(notification: Notification) {
+    
+        let userinfo = notification.userInfo!
+        
+        let keyboardEndFrameScreenCoordinates = (userinfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardframe = self.view.convert(keyboardEndFrameScreenCoordinates, to: view.window)
+        
+        
+        if notification.name == Notification.Name.UIKeyboardWillHide {
+        
+            textView.contentInset = UIEdgeInsets.zero
+        }
+        else {
+        
+            // set bottom insets when keyboard apper
+            textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardframe.height, right: 0)
+            textView.scrollIndicatorInsets = textView.contentInset
+        }
+        
+        textView.scrollRangeToVisible(textView.selectedRange)
+        
+    }
+}
 
+extension ViewController : UITextViewDelegate {
 
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.backgroundColor = UIColor.white
+        textView.textColor = UIColor.black
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        textView.backgroundColor = UIColor.init(red: 51/255, green: 51/255, blue: 51/255, alpha: 1)
+        textView.textColor = UIColor.yellow
+        
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        lb_count.text = "\(textView.text.characters.count)"
+        
+        return textView.text.characters.count + (text.characters.count - range.length) <= 1000
+    }
 }
 
